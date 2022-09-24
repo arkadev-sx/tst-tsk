c51ea8e89c093fd817b0f4fa83f77ec1f1c57bb1
Sergei <56161979+arkadev-sx@users.noreply.github.com>
2022-09-24 15:19:13 +0400

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <GiphyDropdown />
+  </div>
+</template>
+
+<script>
+import GiphyDropdown from "./components/GiphyDropdown.vue";
+
+export default {
+  name: "App",
+  components: {
+    GiphyDropdown,
+  },
+};
+</script>
+<style scoped>
+#app {
+  padding: 8px;
+}
+</style>
---
+++ src/components/GiphyDropdown.vue
@@ -0,0 +1,12 @@
+<template>
+  <input class="giphySearch" />
+</template>
+<script>
+// endpoint: https://api.giphy.com/v1/gifs/search?q=<SEARCH TERM>&api_key=WxdyYLhMuub9clDtbglE0bJkSBExzePj
+export default {};
+</script>
+<style scoped>
+.giphySearch {
+  width: 100%;
+}
+</style>
